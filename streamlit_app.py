dae9d3abdf67fb89eef0b68be97b9b62a79367f3
Micael-Rodrigues <micaelvitor709@gmail.com>
2025-02-11 08:53:39 -0300

    Reload data button added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,54 @@
 import streamlit as st
 import pandas as pd
 import math
-from pathlib import Path
 
-st.set_page_config(
-    page_title="GDP dashboard",
-    page_icon=":earth_americas:",
-)
+if "refresh" not in st.session_state:
+    st.session_state.refresh = 0
+
+if st.button("Atualizar Dados"):
+    st.session_state.refresh += 1
 
 
 @st.cache_data
-def get_gdp_data():
-
-    DATA_FILENAME = "https://raw.githubusercontent.com/m1caelvr/BOT_CEF/refs/heads/main/data/dados.csv"
+def get_gdp_data(dummy):
+    DATA_FILENAME = "data\dados.csv"
     raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
     MIN_YEAR = 1960
     MAX_YEAR = 2022
-
     gdp_df = raw_gdp_df.melt(
         ["Country Code"],
         [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
         "Year",
         "GDP",
     )
-
     gdp_df["Year"] = pd.to_numeric(gdp_df["Year"])
-
     return gdp_df
 
 
-gdp_df = get_gdp_data()
+gdp_df = get_gdp_data(st.session_state.refresh)
 
 min_value = gdp_df["Year"].min()
 max_value = gdp_df["Year"].max()
-
 from_year, to_year = st.slider(
     "Which years are you interested in?",
     min_value=min_value,
     max_value=max_value,
     value=[min_value, max_value],
 )
-
 countries = gdp_df["Country Code"].unique()
-
 if not len(countries):
     st.warning("Select at least one country")
-
 selected_countries = st.multiselect(
     "Which countries would you like to view?",
     countries,
     ["DEU", "FRA", "GBR", "BRA", "MEX", "JPN"],
 )
-
 filtered_gdp_df = gdp_df[
     (gdp_df["Country Code"].isin(selected_countries))
     & (gdp_df["Year"] <= to_year)
     & (from_year <= gdp_df["Year"])
 ]
-
 st.header("GDP over time", divider="gray")
-
 st.line_chart(
     filtered_gdp_df,
     x="Year",
